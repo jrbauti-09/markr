f3b6ebf8a9522a4b1009b2a7f52d65fee0615410
joshua <jrbauti1@asu.edu>
2022-04-27 13:35:32 -0700

    Implemented sign up route and tested validity.

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,6 +1,24 @@
 const express = require("express");
-const router = express.Router();
+const asyncHandler = require("express-async-handler");
 
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+
+const router = express.Router();
 // Will house resources for route paths beginning with /api/users
+// Sign up
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { email, password, username } = req.body;
+    const user = await User.signup({ email, username, password });
+
+    await setTokenCookie(res, user);
+
+    return res.json({
+      user,
+    });
+  })
+);
 
 module.exports = router;
