f10549394b18c03db9051c48632ce18c281512b9
joshua <jrbauti1@asu.edu>
2022-04-27 13:25:45 -0700

    Implemented and tested Login Route

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,12 +1,19 @@
 const asyncHandler = require("express-async-handler");
+const sessionRouter = require("./session.js");
+const usersRouter = require("./users.js");
 
-// User.
-const { User } = require("../../db/models");
+const router = require("express").Router();
 
 // auth middleware.
 const { setTokenCookie } = require("../../utils/auth.js");
+// User.
+const { User } = require("../../db/models");
 
-const router = require("express").Router();
+// Gateway to multiple routes.
+
+// Begins with /api
+router.use("/session", sessionRouter);
+router.use("/users", usersRouter);
 
 router.post("/test", function (req, res) {
   res.json({ requestBody: req.body });
---
+++ backend/routes/api/users.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+
+// Will house resources for route paths beginning with /api/users
+
+module.exports = router;
