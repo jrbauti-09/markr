92f9cb601afc70c2346c62ad07fcf3428e3cc8ef
joshua <jrbauti1@asu.edu>
2022-04-27 18:59:35 -0700

    fixed api and added README

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -15,8 +15,4 @@ const { User } = require("../../db/models");
 router.use("/session", sessionRouter);
 router.use("/users", usersRouter);
 
-router.post("/test", function (req, res) {
-  res.json({ requestBody: req.body });
-});
-
 module.exports = router;
