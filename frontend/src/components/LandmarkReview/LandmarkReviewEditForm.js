ab613d61221c51a95f8c65fa27fc3850cb160c15
joshua <jrbauti1@asu.edu>
2022-05-02 15:15:30 -0700

    review_edit_form has image

--- frontend/src/components/LandmarkReview/LandmarkReviewEditForm.js
+++ frontend/src/components/LandmarkReview/LandmarkReviewEditForm.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router-dom";
 
+import "./LandmarkReviewEditForm.css";
+
 // TODO: import thunk.
 
 export default function LandmarkReviewEditForm() {
@@ -10,10 +12,57 @@ export default function LandmarkReviewEditForm() {
   const history = useHistory();
 
   // we don't need to update userId.
+  // no need to update landMarkId.
 
   const reviewArray = useSelector((state) => Object.values(state.reviews));
-
+  const landMarkObject = useSelector((state) => state.landmarks);
   const landMarkId = reviewArray[0].landMarkId;
+  const landMark = landMarkObject[landMarkId];
 
-  return <div>LandmarkReviewEditForm</div>;
+  const reviewToEdit = reviewArray.find((review) => {
+    return review.id === parseInt(reviewId);
+  });
+
+  const [review, setReview] = useState(reviewToEdit.review);
+  const [validationErrors, setValidationErrors] = useState([]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    window.alert("Review was edited.");
+
+    const data = {
+      review,
+    };
+  };
+
+  useEffect(() => {
+    const errors = [];
+    if (!review.length) errors.push("Please include a review.");
+    setValidationErrors(errors);
+  }, [review]);
+
+  return (
+    <>
+      <div>
+        <img
+          className="review_edit_form_image"
+          src={landMark.imageUrl}
+          alt=""
+        ></img>
+      </div>
+      <div className="review_edit_main_container">
+        <div className="review_edit_form_container">
+          <form className="review_edit_form_holder">
+            <label></label>
+            <textarea
+              type="text"
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+            ></textarea>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
