6d215a2e887d82bfa8345a5074745c79b9104b49
joshua <jrbauti1@asu.edu>
2022-05-02 16:01:31 -0700

    edited css for review form page

--- frontend/src/components/LandmarkReview/LandmarkReviewEditForm.js
+++ frontend/src/components/LandmarkReview/LandmarkReviewEditForm.js
@@ -44,23 +44,52 @@ export default function LandmarkReviewEditForm() {
 
   return (
     <>
-      <div>
-        <img
-          className="review_edit_form_image"
-          src={landMark.imageUrl}
-          alt=""
-        ></img>
-      </div>
-      <div className="review_edit_main_container">
-        <div className="review_edit_form_container">
-          <form className="review_edit_form_holder">
-            <label></label>
-            <textarea
-              type="text"
-              value={review}
-              onChange={(e) => setReview(e.target.value)}
-            ></textarea>
-          </form>
+      <div className="review_edit_form_superdiv">
+        <div>
+          <img
+            className="review_edit_form_image"
+            src={landMark.imageUrl}
+            alt=""
+          ></img>
+        </div>
+        <div className="review_edit_main_container">
+          <div className="review_edit_form_container">
+            <form className="review_edit_form_holder" onSubmit={handleSubmit}>
+              <div className="">
+                <h1 style={{ display: "flex", justifyContent: "center" }}>
+                  Edit your review for {landMark.name}!
+                </h1>
+              </div>
+              <ul className="error_container">
+                {validationErrors.length > 0 &&
+                  validationErrors.map((error) => (
+                    <li className="error" key={error}>
+                      {error}
+                    </li>
+                  ))}
+              </ul>
+              <label style={{ padding: "10px" }}>Review:</label>
+              <textarea
+                type="text"
+                value={review}
+                onChange={(e) => setReview(e.target.value)}
+                style={{
+                  width: "480px",
+                  height: "100px",
+                  padding: "10px",
+                  margin: "10px",
+                  borderRadius: "8px",
+                }}
+              ></textarea>
+              <button
+                className="review_edit_form_button"
+                type="submit"
+                disabled={validationErrors.length > 0}
+              >
+                Edit Review
+              </button>
+            </form>
+          </div>
         </div>
       </div>
     </>
